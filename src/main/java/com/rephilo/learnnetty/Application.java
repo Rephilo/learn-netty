28e5167b48470a30deb9e84c6adae157a36bc65f
874851235@qq.com <123456beijau>
2020-04-06 23:48:20 +0800

    Initial commit

---
+++ src/main/java/com/rephilo/learnnetty/Application.java
@@ -0,0 +1,14 @@
+package com.rephilo.learnnetty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author rephilo
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
